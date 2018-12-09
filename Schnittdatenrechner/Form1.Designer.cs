32e0f92a4a75fab6b7882e4d95230b6f8bbbfe62
Suppennudelchen <carsten@pohland.ch>
2018-12-09 18:34:04 +0100

    Files hochgeladen

---
+++ Schnittdatenrechner/Form1.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace Schnittdatenrechner
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.btnBerechnen = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.textBoxVc = new System.Windows.Forms.TextBox();
+            this.textBoxDm = new System.Windows.Forms.TextBox();
+            this.textBoxDrehz = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblAusgabe = new System.Windows.Forms.Label();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.SuspendLayout();
+            // 
+            // btnBerechnen
+            // 
+            this.btnBerechnen.Enabled = false;
+            this.btnBerechnen.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBerechnen.Location = new System.Drawing.Point(419, 23);
+            this.btnBerechnen.Name = "btnBerechnen";
+            this.btnBerechnen.Size = new System.Drawing.Size(155, 51);
+            this.btnBerechnen.TabIndex = 0;
+            this.btnBerechnen.Text = "Berechnen";
+            this.btnBerechnen.UseVisualStyleBackColor = true;
+            this.btnBerechnen.Click += new System.EventHandler(this.btnBerechnen_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClear.Location = new System.Drawing.Point(419, 80);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(155, 51);
+            this.btnClear.TabIndex = 1;
+            this.btnClear.Text = "Clear";
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClose.Location = new System.Drawing.Point(419, 190);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(155, 51);
+            this.btnClose.TabIndex = 2;
+            this.btnClose.Text = "Beenden";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // textBoxVc
+            // 
+            this.textBoxVc.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxVc.Location = new System.Drawing.Point(38, 44);
+            this.textBoxVc.Name = "textBoxVc";
+            this.textBoxVc.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.textBoxVc.Size = new System.Drawing.Size(100, 30);
+            this.textBoxVc.TabIndex = 3;
+            this.textBoxVc.TextChanged += new System.EventHandler(this.txtEingabe_TextChanged);
+            // 
+            // textBoxDm
+            // 
+            this.textBoxDm.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxDm.Location = new System.Drawing.Point(38, 118);
+            this.textBoxDm.Name = "textBoxDm";
+            this.textBoxDm.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.textBoxDm.Size = new System.Drawing.Size(100, 30);
+            this.textBoxDm.TabIndex = 4;
+            this.textBoxDm.TextChanged += new System.EventHandler(this.txtEingabe_TextChanged);
+            // 
+            // textBoxDrehz
+            // 
+            this.textBoxDrehz.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.textBoxDrehz.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxDrehz.Location = new System.Drawing.Point(38, 200);
+            this.textBoxDrehz.Name = "textBoxDrehz";
+            this.textBoxDrehz.ReadOnly = true;
+            this.textBoxDrehz.Size = new System.Drawing.Size(100, 30);
+            this.textBoxDrehz.TabIndex = 5;
+            this.textBoxDrehz.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(33, 14);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(280, 25);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Schnittgeschwindigkeit [m/min]";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(33, 90);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(264, 25);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Werkzeugdurchmesser [mm]";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(33, 172);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(155, 25);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Drehzahl [1/min]";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.SystemColors.HotTrack;
+            this.label4.Location = new System.Drawing.Point(402, 250);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(179, 17);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "pohland ag - software tools";
+            // 
+            // lblAusgabe
+            // 
+            this.lblAusgabe.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAusgabe.Location = new System.Drawing.Point(33, 242);
+            this.lblAusgabe.Name = "lblAusgabe";
+            this.lblAusgabe.Size = new System.Drawing.Size(329, 23);
+            this.lblAusgabe.TabIndex = 10;
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.BalloonTipText = "Schnittdatenrechner";
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "Pohland";
+            this.notifyIcon1.Visible = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(593, 277);
+            this.Controls.Add(this.lblAusgabe);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxDrehz);
+            this.Controls.Add(this.textBoxDm);
+            this.Controls.Add(this.textBoxVc);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnBerechnen);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Schnittdatenrechner";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.TextBox textBoxVc;
+        private System.Windows.Forms.TextBox textBoxDm;
+        private System.Windows.Forms.TextBox textBoxDrehz;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        internal System.Windows.Forms.Button btnBerechnen;
+        private System.Windows.Forms.Label lblAusgabe;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.ToolTip toolTip1;
+    }
+}
+
---
+++ Schnittdatenrechner/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Schnittdatenrechner
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        //hiermit kann man den Berechnen Knopf Ein und ausschalten
+        //in den Eigenschaften der Label unter TextChanged die Methode aufrufen
+        private void txtEingabe_TextChanged(object sender, EventArgs e)
+        {
+            if (textBoxVc.Text != "" && textBoxDm.Text != "")
+            {
+                btnBerechnen.Enabled = true;
+            }
+            else
+            {
+                btnBerechnen.Enabled = false;
+            }
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            // Die Textboxen bei Klicken des Clear Button leeren
+            textBoxVc.Text = ("");
+            textBoxDm.Text = ("");
+            textBoxDrehz.Text = ("");
+            lblAusgabe.Text = "";
+        }
+
+        private void btnBerechnen_Click(object sender, EventArgs e)
+        {
+            double schnittGeschwindigkeit;
+            double werkzeugDurchmesser;
+            double drehzahl;
+
+            // Eingabe mit try catch prüfen
+            try
+            {
+                //wenn ok, dann wird berechnet
+                lblAusgabe.Text = "";
+                schnittGeschwindigkeit = Convert.ToDouble(textBoxVc.Text);
+                werkzeugDurchmesser = Convert.ToDouble(textBoxDm.Text);
+                drehzahl = (schnittGeschwindigkeit * 1000) / (werkzeugDurchmesser * 3.141);
+                drehzahl = Convert.ToInt32(drehzahl);
+                textBoxDrehz.Text = Convert.ToString(drehzahl);
+            }
+            catch (FormatException)
+            {
+                lblAusgabe.Text = "Falsches Eingabeformat";
+                textBoxVc.Text = ("");
+                textBoxDm.Text = ("");
+                textBoxDrehz.Text = ("");
+            }
+            catch (Exception)
+            {
+                lblAusgabe.Text = "Fehler: Eingaben prüfen";
+                textBoxVc.Text = ("");
+                textBoxDm.Text = ("");
+                textBoxDrehz.Text = ("");
+            }
+        }
+    }
+}
